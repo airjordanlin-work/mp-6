d842d5d5a3dd94c432a198b73b3afe8575f6274e
Airjordanlin-work <jlinwork0605@gmail.com>
2024-12-03 15:44:09 -0500

    Hopefully fixes page 404 error

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -1,19 +1,20 @@
 'use server';
 
-import { signIn } from "@/auth";
+import { signIn, signOut } from "@/auth";
 
 export async function doSocialLogin(formData: FormData): Promise<void> {
     const action = formData.get('action');
 
     // Ensure 'action' is a string before passing it to signIn
     if (typeof action === "string") {
-        await signIn(action, { redirectTo: "/home" });
+        await signIn(action, { redirectTo: "/profile" });
     } else {
         throw new Error("Invalid action parameter in formData");
     }
 }
 
 export async function doLogout(): Promise<void> {
+    await signOut({redirectTo: "/"})
     // Placeholder function for logout logic
     // Implement your logout logic here
 }
