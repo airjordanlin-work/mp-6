4fb208b7b4f205e593dea4bc1dd9721858f632b3
Airjordanlin-work <jlinwork0605@gmail.com>
2024-12-03 13:12:09 -0500

    Added redirect

--- auth.ts
+++ auth.ts
@@ -1,21 +1,26 @@
-import NextAuth from "next-auth";
-import GitHub from "@auth/core/providers/github";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
-export const { handlers, auth, signIn, signOut} = NextAuth({
+const options: NextAuthOptions = {
     providers: [
-        GitHub({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
             authorization: {
                 params: {
                     prompt: "consent",
                     access_type: "offline",
                     response_type: "code",
-
-                }
-            }
+                },
+            },
         }),
     ],
-});
+    callbacks: {
+        async redirect(){
+            // Redirect to /profile after successful login
+            return `https://mp-6-phi.vercel.app/profile`;
+        },
+    },
+};
 
+export default NextAuth(options);
